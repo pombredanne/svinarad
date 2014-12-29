d8963200e80c41ee3f03dcaaf8b17f01fd924ac4
Zeljko Nesic <zeljko.nesic@devana.rs>
2014-12-29 08:39:09 +0100

    First Django commit

---
+++ backend/svinarad/settings.py
@@ -0,0 +1,73 @@
+import django.conf.global_settings as DEFAULT_SETTINGS
+import dj_database_url
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+SECRET_KEY = 't%93=f_pslv69z4=cfzh5#ngy6iezxb_#h9@(z!t4dw3jo4!)q'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'svinarad.urls'
+
+WSGI_APPLICATION = 'svinarad.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres:///svinarad')
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'sr-Latn'
+
+TIME_ZONE = 'Europe/Belgrade'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+WSGI_APPLICATION = 'svinarad.wsgi.application'
